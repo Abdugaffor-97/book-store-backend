1e648b53e97dc602033d941081b48781ff6a6023
Abdug'affor Abdurahimov <abdurahimov.97@list.ru>
2020-12-31 08:13:24 +0500

    fixed all deprecation warnings

--- src/models/index.js
+++ src/models/index.js
@@ -2,6 +2,9 @@ const mongoose = require("mongoose");
 const Book = require("./bookModel");
 const Comment = require("./commentModel");
 
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useUnifiedTopology", true);
+
 const connectDB = () => mongoose.connect(process.env.DATABASE_URL);
 
 const models = { Book, Comment };
