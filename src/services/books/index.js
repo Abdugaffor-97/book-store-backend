25719d1bfc4d62d1e235a8bd43cf7d8c1f6b1d2b
Abdug'affor Abdurahimov <abdurahimov.97@list.ru>
2021-01-31 11:31:12 +0500

    Implemented preview route

--- src/services/books/index.js
+++ src/services/books/index.js
@@ -7,11 +7,27 @@ const booksRouter = express.Router();
 booksRouter.get("/", async (req, res, next) => {
   try {
     const query = q2m(req.query);
+
     const total = await BookModel.countDocuments(query.criteria);
     const books = await BookModel.find(query.criteria)
       .skip(query.options.skip)
       .limit(query.options.limit);
     res.send({ next: query.links("", total)["next"], books });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+booksRouter.get("/preview", async (req, res, next) => {
+  try {
+    const history = await BookModel.find({ category: "history" }).limit(10);
+    const romance = await BookModel.find({ category: "romance" }).limit(10);
+    const horror = await BookModel.find({ category: "horror" }).limit(10);
+    const fantasy = await BookModel.find({ category: "fantasy" }).limit(10);
+    const scifi = await BookModel.find({ category: "scifi" }).limit(10);
+
+    res.send([history, romance, horror, fantasy, scifi]);
   } catch (error) {
     console.log(error);
     next(error);
