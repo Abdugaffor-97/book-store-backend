034fa20e935cc3ec8d52e97928f08fed88f6486c
Abdug'affor Abdurahimov <abdurahimov.97@list.ru>
2021-02-16 22:50:45 +0500

    Schema updated

--- src/services/users/schema.js
+++ src/services/users/schema.js
@@ -3,7 +3,7 @@ const bcrypt = require("bcryptjs");
 
 const UserSchema = new Schema(
   {
-    name: { type: String, required: true },
+    username: { type: String, required: true },
     surname: { type: String, required: true },
     password: { type: String, required: true, minlength: 8 },
     email: { type: String, unique: true },
